db7663e68bfcf024d0e22a0f70758528be46bda2
dane.dung <dane.dung@itlvn.com>
2022-10-19 14:31:09 +0700

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace tiendung_Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
